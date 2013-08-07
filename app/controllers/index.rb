212dced04bd6017cdbd26bb37f05aebd12be9700
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-07 10:41:23 -0500

    Migrations complete, routes in progress

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,12 +1,16 @@
 get '/' do
   # let user create new short URL, display a list of shortened URLs
+  erb :index
 end
 
 post '/urls' do
   # create a new Url
+  @url = Url.new_url
+  @url.save
 end
 
 # e.g., /q6bda
 get '/:short_url' do
   # redirect to appropriate "long" URL
+  
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,11 +1,18 @@
 class Url < ActiveRecord::Base
-  before_save :shorten_url
+  before_save :generate_shortened_url
   # Remember to create a migration!
   
-  def shorten_url
-    
+  def generate_shortened_url
+    short = 5.times.map { (rand(0..25) + 97).chr }.join
+    if Url.where(short_name: short).any?
+      generate_shortened_url
+    else
+      self.short_name = short
+    end
   end
 
-  def self.
+  def self.new_url
+    Url.new(params[:long_name])
   end
+
 end
